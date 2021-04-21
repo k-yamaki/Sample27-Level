516ffbbd38609b54c5db8f7def86ea9ca682d0ae
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-04-21 17:12:25 +0900

    Initial Commit

---
+++ Sample27-Level/Sample27_LevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample27_LevelApp.swift
+//  Sample27-Level
+//
+//  Created by keiji yamaki on 2021/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Sample27_LevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
